a71f8170913f22f57f0f7d75f5d936e985a599b2
raycris24 <raycris.maldonado@utreee.com>
2023-01-10 19:46:04 -0400

    button ready

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -2,20 +2,13 @@ import Image from "next/image";
 import React from "react";
 import styles from "./button.module.css";
 
-import muscle from "../assets/images/muscle.png"
+import muscle from "../assets/images/muscle.png";
 
-const button = () => {
+const button = (props: { href: string | undefined }) => {
   return (
-    <a className={styles.container}>
-      <span className={styles.label}>
-        Join us!
-      </span>
-        <Image
-          src={muscle}
-          alt="biceps"
-          width={30}
-          height={30}
-        />
+    <a className={styles.container} href={props.href}>
+      <span className={styles.label}>Join us!</span>
+      <Image src={muscle} alt="biceps" width={30} height={30} />
     </a>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,6 @@
 import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "@next/font/google";
 import styles from "../styles/Home.module.css";
 import Button from "../components/button";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -16,7 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <Button />
+        <Button href={undefined} />
       </main>
     </>
   );
