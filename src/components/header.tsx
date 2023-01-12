c6f12a101e0765bd3c1a8ee17fd1adf0b856ed31
raycris24 <raycris.maldonado@utreee.com>
2023-01-12 15:40:17 -0400

    done

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
-import Logo from "../assets/images/logo-trainer.png";
 
+import Logo from "../assets/images/logo-trainer.png";
 import Phone from "../assets/images/phone.png";
 import Message from "../assets/images/message.png";
 
@@ -9,15 +9,15 @@ import styles from "./header.module.css";
 const header = () => {
   return (
     <header className={styles.container}>
-      <Image src={Logo} alt="Wolf" width={140} height={64} />
+      <Image src={Logo} alt="Wolf" width={88} height={88} />
       <div className={styles.infoContainer}>
-        <p>
-          <span>
-            <Image src={Phone} alt="Phone" width={16} height={16} />
+        <p className={styles.contactInfo}>
+          <span className={styles.phone}>
+            <Image src={Phone} alt="Phone" width={18} height={18} />
             1829-819-1370
           </span>
-          <span>
-            <Image src={Message} alt="Message" width={16} height={16} />
+          <span className={styles.message}>
+            <Image src={Message} alt="Message" width={18} height={18} />
             <a href="mailto:clients@wolf.fit">clients@wolf.fit</a>
           </span>
         </p>
